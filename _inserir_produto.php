45eb6644c2fcaf9400e15cceec80224baba1bd11
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-01 22:20:50 -0300

    primeiro commit

---
+++ _inserir_produto.php
@@ -0,0 +1,10 @@
+<?php
+$nrproduto = $_POST['nrproduto'];
+$produto = $_POST['produto'];
+$quantidade = $_POST['quantidade'];
+$categoria = $_POST['categoria'];
+$fornecedor = $_POST['fornecedor'];//dando erro nesta linha??
+
+echo $produto;
+
+?>  
