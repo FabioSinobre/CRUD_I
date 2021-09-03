00bd1f5e76c98525a47e263699d09a500d86d87b
FabioSinobre <engenheiro.computacao4.0@gmail.com>
2021-09-02 21:52:27 -0300

    up

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -11,5 +11,11 @@ $fornecedor = $_POST['fornecedor'];
 $sql = "INSERT INTO `_estoque`(`nr_produtos`, `nome_produto`, `categoria`, `quantidade`, `fornecedor`) VALUES ($nrproduto,'$produto',$quantidade,'$categoria','$fornecedor')";
 $inserir = mysqli_query($conexao, $sql);
 
-
 ?>  
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<div class="conteiner" style="width: 300px">
+	<h4>Produto adicionado com sucesso!</h4>
+	<div style="padding-top: 20px">
+		<a href="index.html" role="button" class="btn btn-sm btn-primary">Novo cadastro</a>
+	</div>
+</div>
